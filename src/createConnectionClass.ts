d5eb52f595cf4cc149e616f2f757ca68cbca76ed
minhloi <jimmyloi92@yahoo.com>
2019-09-08 13:29:19 -0700

    Allow passing in aws config instead

---
+++ src/createConnectionClass.ts
@@ -0,0 +1,46 @@
+/**
+ * A connection handler for Amazon ES.
+ *
+ * Uses the aws-sdk to make signed requests to an Amazon ES endpoint.
+ *
+ * @param client {Client} - The Client that this class belongs to
+ * @param config {Object} - Configuration options
+ * @param [config.protocol=http:] {String} - The HTTP protocol that this connection will use, can be set to https:
+ * @class HttpConnector
+ */
+
+import * as AWS from 'aws-sdk';
+import * as aws4 from "aws4";
+import * as http from 'http';
+import { Connection } from '@elastic/elasticsearch'
+
+interface RequestOptions extends http.ClientRequestArgs {
+  asStream?: boolean;
+}
+
+export function createConnectionClass(awsConfig: AWS.Config): any {
+
+  return class extends Connection {
+
+    public request(params: RequestOptions, cb: (err: Error | null, response: http.IncomingMessage | null) => void): http.ClientRequest  {
+
+      const originalMakeRequest = this.makeRequest
+
+      this.makeRequest = (reqParams: http.ClientRequestArgs ) => {
+
+        const options = {
+          host: this.url.href,
+          region: awsConfig.region
+        }
+
+        aws4.sign(options, awsConfig.credentials)
+
+        Object.assign(reqParams, options)
+
+        return originalMakeRequest(reqParams)
+      }
+
+      return super.request(params, cb)
+    }
+  }
+}
